f1fbba1a9f62849a8fa357fd842887d9bb74263c
Henry L. Gómez <henryl.gomez@gmail.com>
2020-06-24 19:07:31 -0500

    Add simulator to the application

---
+++ src/soccerleague/simulator/model/MatchResume.java
@@ -0,0 +1,12 @@
+package soccerleague.simulator.model;
+
+public interface MatchResume {
+    //Información relacionada con el resumen del partido simulado
+
+    String getEventForMinute(int minute);
+
+    int getLocalScore();
+    int getVisitorScore();
+
+    String getFullResume();
+}
